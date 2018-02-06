0934a9a27b9d4a2ccb9c1d1791067ca4a464eec5
diegoc16 <31931736+diegoc16@users.noreply.github.com>
2018-02-06 16:59:20 +0100

    Update skift-host.sh

--- skift-host.sh
+++ skift-host.sh
@@ -3,13 +3,21 @@
 variable="$(hostname)" 
 echo 'Current host name:'$variable
 
-MAC2="$(cat /sys/class/net/eth0/address | cut -c13-17)"
-compare='RPI-'$MAC2
+
+MAC1="$(cat /sys/class/net/eth0/address)"
+
+echo 'MAC address: '$MAC1
+MAC2="$(cat /sys/class/net/eth0/address | cut -c13-14)"
+MAC3="$(cat /sys/class/net/eth0/address | cut -c16-17)"
+compare='RPI-'$MAC2$MAC3
 echo 'Desired hostname: '$compare
 
+
+
+
 if [ $variable != $compare ]; then
-				
-				echo 'inside if'
+
+        echo 'inside if'
         echo '-----task-----'
         echo '-----task-----'
         echo '-----task-----'
