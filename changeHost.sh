2e5abe7b700fe80cfb2b8a4449915556bcd15543
diegoc16 <31931736+diegoc16@users.noreply.github.com>
2018-02-13 13:03:40 +0100

    Update changeHost.sh

--- changeHost.sh
+++ changeHost.sh
@@ -6,7 +6,6 @@ echo $dateRun '  Runned  /etc/Telenor/changeHost.sh'
 actual="$(hostname)"
 #echo 'Current host name:  '$actual
 
-
 MAC1="$(cat /sys/class/net/eth0/address)"
 
 #echo 'MAC address: '$MAC1
@@ -15,23 +14,19 @@ MAC3="$(cat /sys/class/net/eth0/address | cut -c16-17)"
 desired='RPI-'$MAC2$MAC3
 #echo 'Desired hostname: '$desired
 
-
-
-
 if [ $actual != $desired ]; then
 
 #       echo 'inside if'
 #       echo '-----task-----'
 #       echo '-----task-----'
 #       echo '-----task-----'
-        sed -i "s/$actual/$desired/g" /etc/hosts
-        sed -i "s/$actual/$desired/g" /etc/hostname
+#       sed -i "s/$actual/$desired/g" /etc/hosts
+#       sed -i "s/$actual/$desired/g" /etc/hostname
         echo '    changed from '$actual' to '$desired
         echo '    if is working well , just uncomment the  commands in /etc/Tel$
-        reboot
+#       reboot
 
 fi
-
 
 #echo 'Done'
 
