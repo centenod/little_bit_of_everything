6e74c13747e930be28368f53e4573afa8249f0d9
diegoc16 <31931736+diegoc16@users.noreply.github.com>
2018-02-13 13:02:09 +0100

    Update eraseLogs.sh

--- eraseLogs.sh
+++ eraseLogs.sh
@@ -2,15 +2,11 @@
 
 
 echo 'Erasing'
-sudo rm /etc/Telenor/logChangehost.log
-sudo rm /etc/Telenor/logRunudpClient.log
+sudo rm /etc/Telenor/logChangeHost.log
+sudo rm /etc/Telenor/logRunUdpClient.log
 sudo rm /etc/Telenor/logRunUdpServer.log
-
-
 echo ''
 echo 'Change host log  ERASED '
-
 echo 'Run UDP client log  ERASED '
-
 echo 'Run UDP server log  ERASED '
 
