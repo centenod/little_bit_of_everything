b54abf54029d5ffc382be65dd913251c1270d593
Laassee <lassejoerg@gmail.com>
2018-02-13 08:45:50 +0100

    Update and rename skift-host.sh to changehost.sh

---
+++ changehost.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+dateRun="$(date)"
+echo $dateRun '  Runned  /etc/Telenor/changehost.sh'
+
+actual="$(hostname)"
+#echo 'Current host name:  '$actual
+
+
+MAC1="$(cat /sys/class/net/eth0/address)"
+
+#echo 'MAC address: '$MAC1
+MAC2="$(cat /sys/class/net/eth0/address | cut -c13-14)"
+MAC3="$(cat /sys/class/net/eth0/address | cut -c16-17)"
+desired='RPI-'$MAC2$MAC3
+#echo 'Desired hostname: '$desired
+
+
+
+
+if [ $actual != $desired ]; then
+
+#       echo 'inside if'
+#       echo '-----task-----'
+#       echo '-----task-----'
+#       echo '-----task-----'
+        sed -i "s/$actual/$desired/g" /etc/hosts
+        sed -i "s/$actual/$desired/g" /etc/hostname
+        echo '    changed from '$actual' to '$desired
+        echo '    if is working well , just uncomment the  commands in /etc/Tel$
+        reboot
+
+fi
+
+
+#echo 'Done'
+
+
+
+
