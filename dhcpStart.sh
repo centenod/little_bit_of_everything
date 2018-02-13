20b786c993d64b41865dac25532dc7f7c0f12e9b
diegoc16 <31931736+diegoc16@users.noreply.github.com>
2018-02-13 10:31:59 +0100

    Create dhcpStart.sh

---
+++ dhcpStart.sh
@@ -0,0 +1,9 @@
+/etc/Telenor/dhcpStart.sh
+
+#!/bin/bash
+
+macadr="$(cat /sys/class/net/eth0/address)"
+sudo sed -i "s/thismacaddress/$macadr/g" /etc/dhcp/dhcpd.conf
+
+
+# now it should start the DHCP service
