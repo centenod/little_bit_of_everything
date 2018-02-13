6e1463daaac227ffaed9a36b33e2d5b2a322e567
Laassee <lassejoerg@gmail.com>
2018-02-13 08:53:53 +0100

    Update runudpclient.sh

--- runudpclient.sh
+++ runudpclient.sh
@@ -2,10 +2,15 @@
 # lunch  UDP client python program 12 times in a
 # minute once every 5 secondsc/path/to/task
 
-(sleep 9 && python3 /etc/TelenorClient/udpClient.py) &
-(sleep 18 && python3 /etc/TelenorClient/udpClient.py) &
-(sleep 26 && python3 /etc/TelenorClient/udpClient.py) &
-(sleep 34 && python3 /etc/TelenorClient/udpClient.py) &
-(sleep 41 && python3 /etc/TelenorClient/udpClient.py) &
-(sleep 48 && python3 /etc/TelenorClient/udpClient.py) &
-(sleep 54 && python3 /etc/TelenorClient/udpClient.py) &
+dateRun="$(date)"
+echo $dateRun '  Runned  /etc/Telenor/runudpclient.sh'
+
+(sleep 9 && python3 /etc/Telenor/udpClient.py) &
+(sleep 18 && python3 /etc/Telenor/udpClient.py) &
+(sleep 26 && python3 /etc/Telenor/udpClient.py) &
+(sleep 34 && python3 /etc/Telenor/udpClient.py) &
+(sleep 41 && python3 /etc/Telenor/udpClient.py) &
+(sleep 48 && python3 /etc/Telenor/udpClient.py) &
+(sleep 54 && python3 /etc/Telenor/udpClient.py) &
+
+exit 0
