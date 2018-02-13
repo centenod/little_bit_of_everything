c959d8eacac69832e084f6012fe61e3605bdcff2
Laassee <lassejoerg@gmail.com>
2018-02-13 08:57:12 +0100

    Add files via upload

---
+++ eraselogs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo 'Erasing'
+sudo rm /etc/Telenor/logChangehost.log
+sudo rm /etc/Telenor/logrunudpclient.log
+sudo rm /etc/Telenor/logrunudpserver.log
+
+
+echo ''
+echo 'Change host log  ERASED '
+
+echo 'Run UDP client log  ERASED '
+
+echo 'Run UDP server log  ERASED '
+
