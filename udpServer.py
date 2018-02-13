a55fafb3382a27267159f14ac1103835965fb482
Laassee <lassejoerg@gmail.com>
2018-02-13 09:26:42 +0100

    Update udpServer.py

--- udpServer.py
+++ udpServer.py
@@ -1,4 +1,4 @@
-import socket
+mport socket
 import time
 from updatePiList import updatePiList
 
@@ -7,9 +7,13 @@ def Main():
 
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.bind(('',port))
+        print()
+        print(" ---------------------------------------------------- ")
+        ts = time.asctime ( time.localtime(time.time()))
+        print("         Server started!  "+ ts )
+        print(" ---------------------------------------------------- ")
 
-	ts = time.asctime ( time.localtime(time.time()))
-        print(" '/n' Server started! + ts")
+
         while True:
                 data, addr = s.recvfrom(1024)
                 data = data.decode('utf-8')
@@ -19,12 +23,12 @@ def Main():
                         if char in "( )'":
                                 address = address.replace(char,'')
                 ts = time.asctime ( time.localtime(time.time()))
-                status = (data + "," + address + "," + "UP" + "," + "3" + "," + ts)
+                status = (data + "," + address + "," + "UP" + "," + "3" + "," +$
                 l = status.split(',')
                 l.remove('5001')
                 print(l)
                 updatePiList(l)
         c.close()
-	
+
 if __name__ == '__main__':
         Main()
