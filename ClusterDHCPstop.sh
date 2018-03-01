0eda4dda04092205a680a067bbed9450fe079db9
diegoc16 <31931736+diegoc16@users.noreply.github.com>
2018-03-01 10:30:22 +0100

    Update and rename ClusterDHCPstop to ClusterDHCPstop.sh

---
+++ ClusterDHCPstop.sh
@@ -0,0 +1,13 @@
+
+
+# ClusterDHCPstop.sh
+# 
+# 1.Stop the dhcp-Server 
+# 2.Delete the static IP file from the system and replace it 
+# with a empty file of a file that has dhcp configured for 
+# the interfaces.
+# 
+
+
+
+
