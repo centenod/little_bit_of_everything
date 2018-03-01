d6e59a5081d836546dbabce091ff7447ce7ec8a3
diegoc16 <31931736+diegoc16@users.noreply.github.com>
2018-03-01 10:28:12 +0100

    Update and rename ClusterDHCPstart to ClusterDHCPstart.sh

---
+++ ClusterDHCPstart.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# ClusterDHCPstart.sh :
+# 1. Delete the interface system file and replace it with a
+# file with the static IP used by the dhcpd.conf file.
+# 2. A dhcpd.conf file was previously configured and copy with
+# the dhcp parameters to the dhcp system folder. This file is 
+# just used when the dhcp-server is in use.
+# 3. Launch the dhcp-Server
+
+#--------------------------Delete and copy the interface file
+sudo cp 
+sudo rm 
+
+#-------------------------If the dhcpd.conf file wanna be changed every time this can be used.
+sudo cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf_backupfile
+sudo rm /etc/dhcp/dhcpd.conf
+sudo cp /etc/Telenor/dhcpd.conf /etc/dhcp/
+
+
+#-------------------------run the DHCP service
+
+sudo systemctl restart isc-dhcp-server
