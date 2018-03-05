b6bb858078907671a3d351b58255cb2f86f5a379
Laassee <lassejoerg@gmail.com>
2018-03-05 12:44:31 +0100

    Update countDown.py

--- countDown.py
+++ countDown.py
@@ -4,18 +4,23 @@ import csv
 # This program should be launch every minute
 # What if the file is empty????
 # it should do nothing
+zero = "0"
+one = "1"
+two = "2"
+three = "3"
+down="DOWN"
+with open('piList.csv','r',newline='') as infile, open('count.csv','w',newline='') as outfile:
+        fieldnames = ['Hostname', 'IP', 'Status', 'Countdown','Last update']
+        r = csv.DictReader(infile, fieldnames=fieldnames, dialect='excel-tab')
+        w = csv.DictWriter(outfile, fieldnames=fieldnames, dialect='excel-tab')
+        for row in r:
+                if row['Countdown'] == two or row['Countdown'] == three:
+                        temp_count= int(row['Countdown']) - 1
+                        w.writerow({'Hostname' : row['Hostname'], 'IP' : row['IP'], 'Status' : row['Status'], 'Countdown' : temp_count, 'Last upda$
 
-	with open('piList.csv','r',newline='') as infile, open('count.csv','w',newline='') as outfile:
-		fieldnames = ['Hostname', 'IP', 'Status', 'Countdown','Last update']
-		r = csv.DictReader(infile, fieldnames=fieldnames, dialect='excel-tab')
-		w = csv.DictWriter(outfile, fieldnames=fieldnames, dialect='excel-tab')
-		for row in r:
-			if row['Countdown'] >= 1:
-				temp_count= int(row['Countdown']) - 1 
-				w.writerow({'Hostname' : row['Hostname'], 'IP' : row['IP'], 'Status' : row['Status'], 'Countdown' : temp_count, 'Last update' : row['Last update']})
-
-			elseif row['Hostname'] ==0:
-				w.writerow({'Hostname' : row['Hostname'], 'IP' : row['IP'], 'Status' : "DOWN", 'Countdown' : temp_count, 'Last update' : row['Last update']})
-
-			
-	os.replace('count.csv','piList.csv')
+                elif row['Countdown'] == one:
+                        temp_count= int(row['Countdown']) - 1
+                        w.writerow({'Hostname' : row['Hostname'], 'IP' : row['IP'], 'Status' : down, 'Countdown' : temp_count , 'Last update' : ro$
+                else:
+                        w.writerow(row)
+os.replace('count.csv','piList.csv')
