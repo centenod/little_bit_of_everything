eedb1f74d704a82df30c14879ea8b1237ea116cf
diegoc16 <31931736+diegoc16@users.noreply.github.com>
2018-03-05 10:30:35 +0100

    Update countDown.py

--- countDown.py
+++ countDown.py
@@ -1,45 +1,21 @@
 import os
 import csv
 
+# This program should be launch every minute
+# What if the file is empty????
+# it should do nothing
 
-def updatePiList(msg):
-
-	fileMissing = os.path.isfile('piList.csv')
-
-	with open('piList.csv','a') as appendfile:
-		fieldnames = ['Hostname', 'IP', 'Status', 'Countdown','Last update']
-		w = csv.DictWriter(appendfile, fieldnames=fieldnames, dialect='excel-tab')
-		#WRITING THE HEADERS IN THE .CSV-FILE IF THE FILE IS EMPTY
-		if not fileMissing:
-			w.writeheader()
-
-	with open('piList.csv','r',newline='') as readfile, open('piList.csv','a',newline='') as appendfile:
-		fieldnames = ['Hostname', 'IP', 'Status', 'Countdown','Last update']
-		r = csv.DictReader(readfile, fieldnames=fieldnames, dialect='excel-tab')
-		w = csv.DictWriter(appendfile, fieldnames=fieldnames, dialect='excel-tab')
-		k=0
-		for row in r: 
-		#IF NO CURRENT ENTRY IN THE .CSV-FILE -> APPEND THEN STOP SCRIPT UNTIL NEXT ENTRY
-			if 'RPI-' in msg[0] and msg[0] in row['Hostname']:
-				k=0
-				break
-			else:
-				k=1
-		if 'RPI-' in msg[0] and k==1:
-			w.writerow({'Hostname' : msg[0], 'IP' : msg[1], 'Status' : msg[2], 'Countdown' : msg[3], 'Last update' : msg[4]})
-
-			quit
-
-
-	with open('piList.csv','r',newline='') as infile, open('out.csv','w',newline='') as outfile:
+	with open('piList.csv','r',newline='') as infile, open('count.csv','w',newline='') as outfile:
 		fieldnames = ['Hostname', 'IP', 'Status', 'Countdown','Last update']
 		r = csv.DictReader(infile, fieldnames=fieldnames, dialect='excel-tab')
 		w = csv.DictWriter(outfile, fieldnames=fieldnames, dialect='excel-tab')
 		for row in r:
-			#lOOKING FOR MATCHING "HOSTNAME" FOR EVERY LINE. 
-			#IF MATCH IS FOUND, UPDATE THE WHOLE LINE WITH NEW INFO, IF NOT, USE OLD LINE
-			if ('RPI-' in msg[0]) and (row['Hostname'] == msg[0]):
-				w.writerow({'Hostname' : msg[0], 'IP' : msg[1], 'Status' : msg[2], 'Countdown' : msg[3], 'Last update' : msg[4]})
-			else:
-				w.writerow(row)
-	os.replace('out.csv','piList.csv')
+			if row['Countdown'] >= 1:
+				temp_count= int(row['Countdown']) - 1 
+				w.writerow({'Hostname' : row['Hostname'], 'IP' : row['IP'], 'Status' : row['Status'], 'Countdown' : temp_count, 'Last update' : row['Last update']})
+
+			elseif row['Hostname'] ==0:
+				w.writerow({'Hostname' : row['Hostname'], 'IP' : row['IP'], 'Status' : "DOWN", 'Countdown' : temp_count, 'Last update' : row['Last update']})
+
+			
+	os.replace('count.csv','piList.csv')
