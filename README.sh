1338896179faf034ef1df692b15142daa285b2e4
diegoc16 <31931736+diegoc16@users.noreply.github.com>
2018-03-06 09:28:49 +0100

    Update and rename README.txt to README.sh

--- README.sh
+++ README.sh
@@ -2,22 +2,38 @@
 little_bit_of_everything
 
 
-Steps to fully configure a node:
+###### Steps to fully configure a node:
 
-1. Update and upgrade the OS.
-2. Download the files from GitHub:
+###### 1. Update and upgrade the OS.
+###### 2. Download the files from GitHub:
 $ git clone git://github.com/Laassee/masterT /home/pi
-3. Move the folder "telenor" :
+###### 3. Move the folder "telenor" :
 $ mv /home/pi/masterT/t* /home/pi/
-4. Delete the folder downloaded from GitHub:
+###### 4. Delete the folder downloaded from GitHub:
 $sudo rm -r /home/pi/masterT/
-5. Install applications and programs used:
+###### 5. Install applications and programs used:
 $ sudo apt-get update && sudo apt-get upgrade -y
 $ sudo pip3 install netifaces 
 $ sudo easy_install3 pip 
 $ sudo apt-get install -y isc-dhcp-server 
 $ sudo apt-get install -y nmap
 
+(sudo crontab -l 2>/dev/null; echo "* * * * * myscript")| sudo crontab - 
+(sudo crontab -l ; echo "* * * * * myscript")| sudo crontab -
+
+
+###### 7. Give permissions to the files to write logs 
+$ sudo chmod 777 /home/pi/telenor/changeHost.sh
+$ sudo chmod 777 /home/pi/telenor/runUdpClient.sh
+$ sudo chmod 777 /home/pi/telenor/udpServer.py
+$ sudo chmod 777 /home/pi/telenor/updatePiList.py
+$ sudo chmod 777 /home/pi/telenor/countDown.py
+
+###### Replace existing DHCP server configuration with custom from ./telenor/network 
+$ sudo rm /etc/dhcp/dhcpd.conf && sudo cp /home/pi/telenor/network/custom_dhcpd.conf /etc/dhcp/dhcpd.conf 
+
+###### Update which inteface the DHCP service should work with (replace existing file): 
+$ sudo rm /etc/default/isc-dhcp-server && sudo cp /home/pi/telenor/network/isc-dhcp-server /etc/default/isc-dhcp-server
 
 
 
