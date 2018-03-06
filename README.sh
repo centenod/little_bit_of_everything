8004ec4b056457138e6d7b059c9947299d1fc5a2
diegoc16 <31931736+diegoc16@users.noreply.github.com>
2018-03-06 09:36:11 +0100

    Update README.sh

--- README.sh
+++ README.sh
@@ -9,7 +9,7 @@
 $ sudo apt-get update && sudo apt-get upgrade -y
 
 ###### 2. Download the files from GitHub:
-$ git clone git://github.com/Laassee/masterT /home/pi
+$ git clone git://github.com/Laassee/masterT /home/pi/
 
 ###### 3. Move the folder "telenor" and  Delete the folder downloaded from GitHub:
 $ mv /home/pi/masterT/t* /home/pi/ && sudo rm -r /home/pi/masterT/
@@ -36,48 +36,3 @@ $ sudo rm /etc/dhcp/dhcpd.conf && sudo cp /home/pi/telenor/network/custom_dhcpd.
 
 ###### 8. Update which inteface the DHCP service should work with (replace existing file): 
 $ sudo rm /etc/default/isc-dhcp-server && sudo cp /home/pi/telenor/network/isc-dhcp-server /etc/default/isc-dhcp-server
-
-
-
-
-
-
-
-Downloading the files:
-
-
-wget ______________path____________ ____location______________
-
-also install:
-
-sudo apt-get update
-sudo apt-get upgrate
-sudo pip3 install netifaces
-sudo easy_install3 pip
-sudo apt-get install -y isc-dhcp-server
-sudo apt-get install -y nmap
-
-
-
-for running the udp client file every minute with space of 5 seconds in bettew go to crontab and add the following line at the bottom of the file
-
-$ crontab -e
-add the following line at the bottom:
-
-
-* * * * * /etc/Telenor/runudpclient.sh >> /etc/Telenor/logrunudpclient.log 2>&1
-@reboot /etc/Telenor/changehost.sh >> /etc/Telenor/logChangehost.log 2>&1
-@reboot /etc/Telenor/runudpserver.sh >> /etc/Telenor/logudpserver.log 2>&1
-
-
-/etc/Telenor contains:
-updClient.py
-udpServer.py
-updatePiList.py
-piList.csv
-runUdpClient.sh
-runUdpServer.sh
-changeHost.sh
-
-pi@raspberrypi:/etc/Telenor $ sudo chown pi:pi /etc/Telenor
-sudo chmod 777 "files that need permision"
